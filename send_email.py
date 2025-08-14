e21978419fce501a2a5f6bf50ee5c2ffb345f4d4
aidantze <122945487+aidantze@users.noreply.github.com>
2025-08-14 11:41:35 +1000

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,7 +6,7 @@ mail= Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'testmycode159@gmail.com'
+app.config['MAIL_USERNAME'] = 'email@email.com'
 app.config['MAIL_PASSWORD'] = 'qwertyuiop[]\\'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
@@ -14,7 +14,7 @@ mail = Mail(app)
 
 @app.route("/sendemail")
 def index():
-   msg = Message('Hello', sender = 'testmycode159@gmail.com', recipients = ['testmycode159@gmail.com'])
+   msg = Message('Hello', sender = 'email@email.com', recipients = ['email@email.com'])
    msg.body = "Hello Flask message sent from Flask-Mail"
    mail.send(msg)
    return "Sent"
